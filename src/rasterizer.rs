38a818cb5c219eca1ed8aa2b74ded0a774478e9c
Shritesh Bhattarai <shr@ite.sh>
2023-10-22 08:04:12 -0700

    not a fan of casting

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -1,16 +1,15 @@
 use crate::{Canvas, Color};
 
-fn interpolate(i0: i32, d0: i32, i1: i32, d1: i32) -> Vec<(i32, f64)> {
-    let a = (d1 - d0) as f64 / (i1 - i0) as f64;
-
+fn interpolate(i0: i32, d0: i32, i1: i32, d1: i32) -> Vec<f64> {
     let mut values = Vec::new();
 
     if i0 == i1 {
-        values.push((i0, d0 as f64));
+        values.push(d0 as f64);
     } else {
+        let a = (d1 - d0) as f64 / (i1 - i0) as f64;
         let mut d = d0 as f64;
-        for i in i0..=i1 {
-            values.push((i, d));
+        for _ in i0..=i1 {
+            values.push(d);
             d += a;
         }
     }
@@ -22,10 +21,7 @@ pub fn draw_line(canvas: &mut Canvas, p0: (i32, i32), p1: (i32, i32), color: Col
     let (mut x0, mut y0) = p0;
     let (mut x1, mut y1) = p1;
 
-    let dx = x1 - x0;
-    let dy = y1 - y0;
-
-    if dx.abs() > dy.abs() {
+    if (x1 - x0).abs() > (y1 - y0).abs() {
         // line is horizontal-ish
 
         if x0 > x1 {
@@ -33,17 +29,20 @@ pub fn draw_line(canvas: &mut Canvas, p0: (i32, i32), p1: (i32, i32), color: Col
             std::mem::swap(&mut y0, &mut y1);
         }
 
-        for (x, y) in interpolate(x0, y0, x1, y1) {
-            canvas.put_pixel(x, y as i32, color);
+        let ys = interpolate(x0, y0, x1, y1);
+
+        for x in x0..=x1 {
+            canvas.put_pixel(x, ys[(x - x0) as usize] as i32, color);
         }
     } else {
         if y0 > y1 {
             std::mem::swap(&mut x0, &mut x1);
             std::mem::swap(&mut y0, &mut y1);
         }
+        let xs = interpolate(y0, x0, y1, x1);
 
-        for (y, x) in interpolate(y0, x0, y1, x1) {
-            canvas.put_pixel(x as i32, y, color);
+        for y in y0..=y1 {
+            canvas.put_pixel(xs[(y - y0) as usize] as i32, y, color);
         }
     }
 }
