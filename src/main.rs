6ee1b672de235789daa82c5475d885365c04d0f4
Shritesh Bhattarai <shr@ite.sh>
2023-10-22 09:18:15 -0700

    Filled triangle

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,24 @@ mod vec3;
 
 use canvas::Canvas;
 use color::Color;
+use rasterizer::Point;
 
 fn main() {
     let mut canvas = Canvas::new("Computer Graphics from Scratch", 800, 800);
 
+    rasterizer::draw_filled_triangle(
+        &mut canvas,
+        Point(-200, -250),
+        Point(200, 50),
+        Point(20, 250),
+        Color(0, 255, 0),
+    );
+
     rasterizer::draw_wireframe_triangle(
         &mut canvas,
-        (-200, -250),
-        (200, 50),
-        (20, 250),
+        Point(-200, -250),
+        Point(200, 50),
+        Point(20, 250),
         Color(255, 255, 255),
     );
 
--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -1,15 +1,28 @@
 use crate::{Canvas, Color};
 
-fn interpolate(i0: i32, d0: i32, i1: i32, d1: i32) -> Vec<f64> {
+#[derive(Clone, Copy)]
+pub struct Point(pub i32, pub i32);
+
+impl Point {
+    pub fn x(&self) -> i32 {
+        self.0
+    }
+
+    pub fn y(&self) -> i32 {
+        self.1
+    }
+}
+
+fn interpolate(i0: i32, d0: i32, i1: i32, d1: i32) -> Vec<(i32, f64)> {
     let mut values = Vec::new();
 
     if i0 == i1 {
-        values.push(d0 as f64);
+        values.push((i0, d0 as f64));
     } else {
         let a = (d1 - d0) as f64 / (i1 - i0) as f64;
         let mut d = d0 as f64;
-        for _ in i0..=i1 {
-            values.push(d);
+        for i in i0..=i1 {
+            values.push((i, d));
             d += a;
         }
     }
@@ -17,44 +30,71 @@ fn interpolate(i0: i32, d0: i32, i1: i32, d1: i32) -> Vec<f64> {
     values
 }
 
-pub fn draw_line(canvas: &mut Canvas, p0: (i32, i32), p1: (i32, i32), color: Color) {
-    let (mut x0, mut y0) = p0;
-    let (mut x1, mut y1) = p1;
-
-    if (x1 - x0).abs() > (y1 - y0).abs() {
+pub fn draw_line(canvas: &mut Canvas, mut p0: Point, mut p1: Point, color: Color) {
+    if (p1.x() - p0.x()).abs() > (p1.y() - p0.y()).abs() {
         // line is horizontal-ish
 
-        if x0 > x1 {
-            std::mem::swap(&mut x0, &mut x1);
-            std::mem::swap(&mut y0, &mut y1);
+        if p0.x() > p1.x() {
+            std::mem::swap(&mut p0, &mut p1);
         }
 
-        let ys = interpolate(x0, y0, x1, y1);
-
-        for x in x0..=x1 {
-            canvas.put_pixel(x, ys[(x - x0) as usize] as i32, color);
+        for (x, y) in interpolate(p0.x(), p0.y(), p1.x(), p1.y()) {
+            canvas.put_pixel(x, y as i32, color);
         }
     } else {
-        if y0 > y1 {
-            std::mem::swap(&mut x0, &mut x1);
-            std::mem::swap(&mut y0, &mut y1);
+        if p0.y() > p1.y() {
+            std::mem::swap(&mut p0, &mut p1);
         }
-        let xs = interpolate(y0, x0, y1, x1);
 
-        for y in y0..=y1 {
-            canvas.put_pixel(xs[(y - y0) as usize] as i32, y, color);
+        for (y, x) in interpolate(p0.y(), p0.x(), p1.y(), p1.x()) {
+            canvas.put_pixel(x as i32, y, color);
         }
     }
 }
 
-pub fn draw_wireframe_triangle(
-    canvas: &mut Canvas,
-    p0: (i32, i32),
-    p1: (i32, i32),
-    p2: (i32, i32),
-    color: Color,
-) {
+pub fn draw_wireframe_triangle(canvas: &mut Canvas, p0: Point, p1: Point, p2: Point, color: Color) {
     draw_line(canvas, p0, p1, color);
     draw_line(canvas, p1, p2, color);
     draw_line(canvas, p2, p0, color);
+}
+
+pub fn draw_filled_triangle(
+    canvas: &mut Canvas,
+    mut p0: Point,
+    mut p1: Point,
+    mut p2: Point,
+    color: Color,
+) {
+    // sort according to y
+    if p1.y() < p0.y() {
+        std::mem::swap(&mut p1, &mut p0);
+    }
+    if p2.y() < p0.y() {
+        std::mem::swap(&mut p2, &mut p0);
+    }
+    if p2.y() < p1.y() {
+        std::mem::swap(&mut p2, &mut p1);
+    }
+
+    let mut x01 = interpolate(p0.y(), p0.x(), p1.y(), p1.x());
+    let x12 = interpolate(p1.y(), p1.x(), p2.y(), p2.x());
+    let x02 = interpolate(p0.y(), p0.x(), p2.y(), p2.x());
+
+    _ = x01.pop();
+
+    let x012 = [x01, x12].concat();
+
+    let m = x02.len() / 2;
+    let (x_left, x_right) = if x02[m] < x012[m] {
+        (x02, x012)
+    } else {
+        (x012, x02)
+    };
+
+    for ((left_y, left_x), (right_y, right_x)) in x_left.into_iter().zip(x_right) {
+        assert_eq!(left_y, right_y);
+        for x in (left_x as i32)..=(right_x as i32) {
+            canvas.put_pixel(x, left_y, color)
+        }
+    }
 }
