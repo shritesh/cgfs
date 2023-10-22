6cde91f590d273efcc7788dd1c09af3b854d1801
Shritesh Bhattarai <shr@ite.sh>
2023-10-22 09:31:00 -0700

    cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
 mod canvas;
 mod color;
 mod rasterizer;
+mod raytracer;
 mod vec3;
+
+use std::env;
 
 use canvas::Canvas;
 use color::Color;
 use rasterizer::Point;
+use raytracer::Raytracer;
+use vec3::Vec3;
 
 fn main() {
     let mut canvas = Canvas::new("Computer Graphics from Scratch", 800, 800);
 
-    rasterizer::draw_filled_triangle(
-        &mut canvas,
-        Point(-200, -250),
-        Point(200, 50),
-        Point(20, 250),
-        Color(0, 255, 0),
-    );
+    match env::args().skip(1).next().as_deref() {
+        Some("raytrace") => {
+            Raytracer::DEFAULT_SCENE.render(&mut canvas);
+        }
+        Some("rasterize") => {
+            rasterizer::draw_filled_triangle(
+                &mut canvas,
+                Point(-200, -250),
+                Point(200, 50),
+                Point(20, 250),
+                Color(0, 255, 0),
+            );
 
-    rasterizer::draw_wireframe_triangle(
-        &mut canvas,
-        Point(-200, -250),
-        Point(200, 50),
-        Point(20, 250),
-        Color(0, 0, 0),
-    );
+            rasterizer::draw_wireframe_triangle(
+                &mut canvas,
+                Point(-200, -250),
+                Point(200, 50),
+                Point(20, 250),
+                Color(0, 0, 0),
+            );
+        }
+        _ => return eprintln!("specify 'raytrace' or 'rasterize'"),
+    }
 
     canvas.show();
 }
